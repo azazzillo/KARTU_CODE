fbc717a190d17bcc2d83e52ad728f320d08907c9
azazzillo <mariyash_zhumabekova@mail.ru>
2024-11-12 14:23:16 +0600

    admin

--- study_code/auth/admin.py
+++ study_code/auth/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import (
+    Task, CustomUser, Tutor, Student, 
+    Result, Chapter, Subject, Group
+)
 
-# Register your models here.
+
+admin.site.register(Task)
+admin.site.register(CustomUser)
+admin.site.register(Tutor)
+admin.site.register(Student)
+admin.site.register(Result)
+admin.site.register(Chapter)
+admin.site.register(Subject)
+admin.site.register(Group)
