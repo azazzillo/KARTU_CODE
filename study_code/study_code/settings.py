04428ddd8f3ad2118a1c7ce9ad870d39e30131c5
Mariyash <mariyash_zhumabekova@mail.ru>
2024-11-27 01:42:17 +0600

    Update settings.py
    .

--- study_code/study_code/settings.py
+++ study_code/study_code/settings.py
@@ -53,7 +53,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',  # Проверьте этот путь
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination', 
     'PAGE_SIZE': 10,
 }
 
