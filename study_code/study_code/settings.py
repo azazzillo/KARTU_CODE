ecaa7d26970532566051f4fa2df052a34edfa395
Mariyash <mariyash_zhumabekova@mail.ru>
2024-11-30 23:17:06 +0600

    Update settings.py
    environ

--- study_code/study_code/settings.py
+++ study_code/study_code/settings.py
@@ -4,7 +4,7 @@ import os
 import sys
 
 
-SECRET_KEY = config('SECRET_KEY', str)
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': config('DB_PASSWORD', cast=str),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
         'HOST': 'localhost',
         'PORT': '5433',
     }
@@ -108,8 +108,8 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = config('EMAIL_HOST_USER', cast=str)
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', cast=str)
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
 LANGUAGE_CODE = 'en-EN'
 
